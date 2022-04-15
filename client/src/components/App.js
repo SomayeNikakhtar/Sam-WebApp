48ef7791b62bc3755d57fba2d85aecae2043e72c
Somaye Nikakhtar <nikakhtar.somaye@gmail.com>
2022-04-14 22:45:01 -0400

    App (routes)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,55 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AdDetails from "./AdDetails";
+import AdsList from "./AdsList";
+import Footer from "./Footer";
 import GlobalStyles from "./GlobalStyles";
+import Header from "./Header";
+import Homepage from "./Homepage";
+import MyAds from "./MyAds";
+import MyFavs from "./MyFavs";
+import MyMsgs from "./MyMsgs";
+import Post from "./Post";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import { UserProvider } from "./UserContext";
 
 function App() {
     return(
         <BrowserRouter>
             <GlobalStyles/>
-                <Switch>
-                    <Route exact path="/">
-                        {/* <HomePage /> */}
-                    </Route>
-                    <Route exact path="/advertisement-details/:id">
-                        <AdDetails />
-                    </Route>
-                </Switch>
-
+            <UserProvider>
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route exact path="/advertisement-details/:id">
+                    <AdDetails />
+                </Route>
+                <Route exact path="/new">
+                    <Post />
+                </Route>
+                <Route exact path="/motreal-ads">
+                    <AdsList />
+                </Route>
+                <Route exact path="/sign-up">
+                    <SignUp />
+                </Route>
+                <Route exact path="/sign-in">
+                    <SignIn />
+                </Route>
+                <Route exact path="/my-messages">
+                    <MyMsgs />
+                </Route>
+                <Route exact path="/my-ads">
+                    <MyAds />
+                </Route>
+                <Route exact path="/my-favorites">
+                    <MyFavs />
+                </Route>
+            </Switch>
+            <Footer/>
+            </UserProvider>
         </BrowserRouter>
     )
 }
