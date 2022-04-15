173bc699978836feb5f1abd01063de4393bff41c
Somaye Nikakhtar <nikakhtar.somaye@gmail.com>
2022-04-14 23:09:39 -0400

    My-adds page

---
+++ client/src/components/MyAds.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { RiAdvertisementFill,  } from "react-icons/ri";
+import { useHistory } from "react-router-dom";
+import { AiOutlinePlus } from "react-icons/ai";
+import pic from "../assets/house.jpg"
+
+
+const MyAds=()=>{
+    const History=useHistory()
+    return(
+        <Wrapper>
+            <Wrapper2>
+                <Titre>All Ads</Titre>
+                <Icons onClick={()=>History.push('/new')}>
+                    <PlusIcon size="25"/>
+                    <AdIcon size="25" />
+                </Icons>
+            </Wrapper2>
+            <Ad>
+                <Title> westmout- 3-1/2 - blah blah</Title>
+                <Image src={pic}></Image>
+            </Ad>
+        </Wrapper>
+    )
+}
+export default MyAds;
+
+const Wrapper= styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    max-width: 70%;
+    margin-left: 70px;
+    
+`;
+const Wrapper2= styled.div`
+    display: flex;
+    justify-content: space-between;
+    border: none;
+    border-radius: 4px;
+    margin-top: 40px;
+    padding: 20px;
+    box-shadow: 6px 10px 79px 10px rgba(184,178,184,1);
+    
+    
+`;
+const Titre= styled.div`
+
+`;
+const Ad= styled.div`
+    display: flex;
+    justify-content: space-between;
+    border: none;
+    border-radius: 4px;
+    margin-top: 10px;
+    padding: 20px;
+    box-shadow: 6px 10px 79px 10px rgba(184,178,184,1);
+`;
+
+const Icons=styled.div`
+    cursor: pointer;
+        &:hover {
+    
+        }
+`
+const AdIcon=styled(RiAdvertisementFill)`
+    cursor: pointer;
+    color: blue;
+
+`
+const PlusIcon= styled(AiOutlinePlus)`
+    cursor: pointer;
+    color: blue;
+    
+`
+const Title=styled.div`
+    align-self: center;
+`;
+const Image=styled.img`
+    width: 90px;
+`;
