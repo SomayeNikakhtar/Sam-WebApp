ac2b77d54bb99942dfc42d66329e23a334c550ed
Somaye Nikakhtar <nikakhtar.somaye@gmail.com>
2022-04-16 20:11:36 -0400

    modify adsList page

--- client/src/components/AdsList.js
+++ client/src/components/AdsList.js
@@ -1,92 +1,26 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { AdContext } from "./AdContext";
+import Filters from "./Filters";
 
 const AdsList=()=>{
-    const [ads, setAds]=useState([])
-    useEffect(()=>{
-        fetch("/api/ads")
-        .then((res)=>{
-            if (res.ok) {
-                return res.json();
-            }
-            throw Error(res.statusText);
-        })
-        .then (res=> setAds(res.data))
-        .catch(err=>console.error(err))
-    },[])
-    console.log(ads)
+    const {ads}= useContext(AdContext)
 
     if (!ads) return <></>
     return(
     <>
-       
         <Wrapper>
             <RightSide>
-                <Title>Filters</Title>
-                <span>Sort by</span><br/>
-                <select>
-                    <option value="newest">Data listed: Newest first</option>
-                    <option value="lowest">Price: Lowest first</option>
-                    <option value="highest">Price: Highest first</option>
-                </select><br/>
-                <span>Price</span>
-                <FlexDiv> 
-                    <input type="number" placeholder="Min" />  <span> to </span> <input type="number" placeholder="Max" />
-                </FlexDiv>
-                <span>Bedrooms</span><br/>
-                    <select>
-                        <option>All</option>
-                        <option value="stadio">Studio</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="5+">5+</option>
-                    </select><br/> 
-                <span>Bathrooms</span><br/>
-                    <select>
-                        <option>All</option>
-                        <option value="1">1</option>
-                        <option value="1.5">1.5</option>
-                        <option value="2">2</option>
-                        <option value="2.5">2.5</option>
-                        <option value="3">3</option>
-                        <option value="3.5">3.5</option>
-                        <option value="4">4</option>
-                        <option value="4.5">4.5</option>
-                        <option value="5+">5+</option>
-                    </select>
-                <p>Unit Type</p>
-                    <input type="checkbox" value="apartment" id="apartment" name="apartment"/>
-                    <label for="apartment">Apartment</label><br/>
-                    <input type="checkbox" value="condo" id="condo" name="condo"/>
-                    <label for="condo">Condo</label><br/>
-                    <input type="checkbox" value="house" id="house" name="house"/>
-                    <label for="house">House</label><br/>
-                    <input type="checkbox" value="basement" id="basement" name="basement"/>
-                    <label for="basement">Basement</label><br/>
-                <span>Pet Friendly</span><br/>
-                    <input type="radio" value="yes" id="yes" name="yes"/>
-                    <label for="yes">Yes</label><br/>
-                    <input type="radio" value="no" id="no" name="no"/>
-                    <label for="no">No</label><br/>
-                    <input type="radio" value="limited" id="limited" name="limited"/>
-                    <label for="limited">Limited</label><br/> 
-                <span>Parking Included</span><br/>
-                    <input type="radio" value="yes" id="yes" name="yes"/>
-                    <label for="yes">Yes</label><br/>
-                    <input type="radio" value="no" id="no" name="no"/>
-                    <label for="no">No</label><br/>                   
+                <Filters/>
             </RightSide>
             <Main>
                 <Title>All Property Rentals</Title>
                 <AdsContainer>
                     {ads.map((el) => {
                         return (
-                        <ItemContainer to={`/advertisement-details/${el._id}`}>
-                            <Image src={el.imagesSrc[0]} />
+                        <ItemContainer key={el._id} to={`/advertisement-details/${el._id}`}>
+                            <Image src={el.images[0]} />
                             <div>
                                 <AdTitle>{el.title}</AdTitle>
                                 <price>${el.price}</price>
@@ -108,9 +42,7 @@ const Wrapper=styled.div`
     flex-grow: 1;
 `;
 const Title = styled.div`
-  
-  
-  
+
 `;
 const RightSide= styled.div`
     flex-basis: 10%;
@@ -124,9 +56,7 @@ const Main=styled.div`
     padding: 10px;
 
 `;
-const FlexDiv=styled.div`
-    display: flex;
-`;
+
 const AdsContainer = styled.div`
   display: flex;
   flex-direction: row;
